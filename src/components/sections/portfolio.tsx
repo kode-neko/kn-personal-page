ef2469caa568e3cbb8744558860d376709a30584
Kodeneko <ladysun.freedom@gmail.com>
2024-02-27 18:52:39 +0100

    [blueprint] portfolio beta base

--- src/components/sections/portfolio.tsx
+++ src/components/sections/portfolio.tsx
@@ -1,8 +1,34 @@
+import Image from 'next/image'
 import styles from './styles.module.css'
 
+const InfoPortfolio = () => {
+  return (
+    <div>
+      <div>
+        <h3></h3>
+        <ul>social</ul>
+      </div>
+      <p></p>
+      <ul>tags</ul>
+    </div>
+  )
+}
+ 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section>
+      <div>
+        <h2>portfolio</h2>
+        <div>
+          <button>izq</button>
+          <button>der</button>
+        </div>
+        <div>
+          <Image />
+          <InfoPortfolio />
+        </div>
+      </div>
+    </section>
   )
 }
 
