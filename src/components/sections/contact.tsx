0ae47db5b34e7708a32ed36d675cc1bd01dcbfa8
Kodeneko <ladysun.freedom@gmail.com>
2024-02-27 18:58:48 +0100

    [blueprint] beta contact section

--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -1,8 +1,36 @@
+import Image from 'next/image'
 import styles from './styles.module.css'
 
 const Contact = () => {
   return (
-    <div>contact</div>
+    <section>
+      <h2>Contact</h2>
+      <div>
+        <Image />
+      </div>
+      <div>
+        <div>
+          <div>
+            <div>icon</div>
+            <div><input /></div>
+          </div>
+          <div>
+            <div>icon</div>
+            <div><input /></div>
+          </div>
+          <div>
+            <div>icon</div>
+            <div><input /></div>
+          </div>
+          <div>
+            <div>hint</div>
+            <div>
+              <button>enviar</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
