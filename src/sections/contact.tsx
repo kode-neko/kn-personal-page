b46c02735db2e860869be1a8971506aacd8f9474
Kodeneko <ladysun.freedom@gmail.com>
2024-03-02 16:51:11 +0100

    [components] refactor folder sections

--- src/sections/contact.tsx
+++ src/sections/contact.tsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image'
 import styles from './styles.module.css'
-import Icon from '../icon'
+import { Icon } from '@/components'
 
 interface FieldProps {
   icon: string, 
