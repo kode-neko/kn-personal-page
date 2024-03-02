a4cfab72d68a120af608ec972d04a9d51d1d5529
Kodeneko <ladysun.freedom@gmail.com>
2024-03-02 16:52:11 +0100

    [components] storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
+import path from "path";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -15,5 +17,16 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@/components': path.resolve(__dirname, "../src/components"),
+      '@/sections': path.resolve(__dirname, "../src/sections"),
+      '@/models': path.resolve(__dirname, "../src/models"),
+      '@/globals': path.resolve(__dirname, "../src/globals"),
+    };
+
+    return config;
+  }
 };
 export default config;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,6 @@
-export {
-  Contact,
-  Portfolio,
-  Skills,
-  Welcome,
-} from './sections'
 export {default as Footer} from './footer'
 export {default as Icon} from './icon'
-export {default as MainBar} from './main-bar'
+export {
+  MainBar,
+  MainMenu
+} from './main-bar'
