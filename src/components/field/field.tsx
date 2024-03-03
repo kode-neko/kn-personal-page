83c8716e3b89dce2ff9708c94cacef5347b8d83f
Kodeneko <ladysun.freedom@gmail.com>
2024-03-03 21:01:03 +0100

    [compos] form new hook

--- src/components/field/field.tsx
+++ src/components/field/field.tsx
@@ -2,16 +2,23 @@ import Icon from '../icon'
 import styles from './styles.module.css'
 
 interface FieldProps {
+  name: string;
   icon: string, 
   placeholder: string, 
   onChange: () => {}
 }
 
-const Field = ({icon, placeholder, onChange}: FieldProps) => {
+const Field = ({name, icon, placeholder, onChange}: FieldProps) => {
   return (
     <div className={styles.field}>
-      <Icon className={styles.icon} icon={icon} />
-      <input placeholder={placeholder} onChange={onChange}/>
+      <Icon 
+        className={styles.icon} 
+        icon={icon} 
+      />
+      <input 
+        name={name} 
+        placeholder={placeholder} 
+      />
     </div>
   )
 }
