381cee06faf9a2a0db9a8a03269859814b241e7c
Kodeneko <ladysun.freedom@gmail.com>
2024-03-14 20:18:58 +0100

    [sections] singletone & getter IContact

--- src/services/local/ContactLocal.ts
+++ src/services/local/ContactLocal.ts
@@ -2,6 +2,19 @@ import { Contact } from "@/models";
 import IContact from "../IContact";
 
 class ContactLocal implements IContact {
+
+  private static _instance: ContactLocal;
+
+  private constructor() {
+  }
+
+  public static getInstance(): ContactLocal {
+    if(!this._instance) {
+      this._instance = new ContactLocal();
+    }
+    return this._instance;
+  }
+
   newMesvsage(contact: Contact) : Contact {
     return contact;
   }
--- src/services/rest/ContactRest.ts
+++ src/services/rest/ContactRest.ts
@@ -2,6 +2,19 @@ import { Contact } from "@/models";
 import IContact from "../IContact";
 
 class ContactRest implements IContact {
+
+  private static _instance: ContactRest;
+
+  private constructor() {
+  }
+
+  public static getInstance(): ContactRest {
+    if(!this._instance) {
+      this._instance = new ContactRest();
+    }
+    return this._instance;
+  }
+
   newMesvsage(contact: Contact) : Contact {
     return contact;
   }
