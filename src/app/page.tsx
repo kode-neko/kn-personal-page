b99a79f76f1e80dff5c55a2f24e6ace4b095e201
Kodeneko <ladysun.freedom@gmail.com>
2024-03-15 11:40:35 +0100

    [sections] back finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Footer, MainBar } from "@/components";
 import styles from './styles.module.css'
 import { Contact, Portfolio, Skills, Welcome } from "@/sections";
+import clsx from "clsx";
 
 const Home = () => {
   return (
     <div className={styles.mainCont}>
-      <div className={styles.round} />
+      <div className={clsx(styles.round, styles.round01)} />
+      <div className={clsx(styles.round, styles.round02)} />
       <header>
         <MainBar />
       </header>
