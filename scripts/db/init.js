789da528f9c1e17dd7702e45d35b09e5677452d1
Kodeneko <ladysun.freedom@gmail.com>
2024-03-15 17:39:29 +0100

    [service] docker mongo

---
+++ scripts/db/init.js
@@ -0,0 +1,23 @@
+conn = Mongo();
+db = conn.getDB('perpage');
+
+db.mail.insertMany([{
+  name: 'test',
+  mail: 'test@test.es',
+  msg: 'test test test'
+}])
+
+db.createUser({
+  user: 'perpage-admin',
+  pwd: '1234',
+  roles: [
+    { role: 'dbOwner', db: 'perpage' }
+  ]
+});
+db.createUser({
+  user: 'perpage-user',
+  pwd: '1234',
+  roles: [
+    { role: 'readWrite', db: 'perpage' }
+  ]
+})
