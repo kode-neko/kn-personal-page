0d0f8b74c73a6b3f237ff1cdb8dc1d5104ed0ea2
Kodeneko <ladysun.freedom@gmail.com>
2024-04-01 22:32:08 +0200

    [i18n] change lang utils

--- src/actions/lang.ts
+++ src/actions/lang.ts
@@ -2,19 +2,11 @@ enum LangSel {
   EN = 'en',
   ES = 'es'
 }
+const LangList = Object.values(LangSel);
 
 let currentLang = LangSel.EN;
 
-function changeLang(lang: LangSel) {
-  currentLang = lang
-}
-
-function getCurrentLang(): LangSel {
-  return currentLang;
-}
-
 export {
   LangSel,
-  changeLang,
-  getCurrentLang
+  LangList
 }
