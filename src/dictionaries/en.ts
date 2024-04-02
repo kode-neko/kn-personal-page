2d899ac0e7a019ccf12c9bf48405aebe39c42c11
Kodeneko <ladysun.freedom@gmail.com>
2024-04-02 11:30:46 +0200

    [i18n] Translations

--- src/dictionaries/en.ts
+++ src/dictionaries/en.ts
@@ -1,3 +1,47 @@
 export default {
-  welcome: 'Welcome!'
+  mainTitle: 'Welcome!',
+  opts: {
+    welcome: 'Bienvenida',
+    skills: 'Skills',
+    portfolio: 'Portfolio',
+    contact: 'Contact'
+  },
+  skills: {
+    fron: 'Front',
+    back: 'Backend',
+    design: 'Design',
+    tools: 'Tools'
+  },
+  occupation: 'Web/Mobile Developer - Frontend',
+  desc: 'Almost a decade breaking keys to typing awesome code. Fullstack MERN/MEVN',
+  projects: {
+   akkount: {
+    title: 'Akkount',
+    desc: 'Dolore velit Lorem sint eu adipisicing nostrud minim'
+   },
+   cheetty: {
+    title: 'Cheetty',
+    desc: 'Dolore velit Lorem sint eu adipisicing nostrud minim'
+   },
+   lionmiss: {
+    title: 'LionMiss',
+    desc: 'Dolore velit Lorem sint eu adipisicing nostrud minim'
+   },
+   codetest: {
+    title: 'Code\'s Pieces',
+    desc: 'Dolore velit Lorem sint eu adipisicing nostrud minim'
+   },
+   placeholder: {
+    name: 'my name is...',
+    mail: 'mail@mail.es',
+    msg: 'Congrats, critics, bla, bla...'
+   },
+   labels: {
+    send: 'Send'
+   }
+  },
+  msg: {
+    mailSuc: '😃 Message sended',
+    mailFail: '☹️ There was an error. Try again later.'
+  }
 }
--- src/dictionaries/es.ts
+++ src/dictionaries/es.ts
@@ -1,3 +1,47 @@
 export default {
-  welcome: '¡Bienvenido!'
+  mainTitle: '¡Bienvenido!',
+  opts: {
+    welcome: 'Bienvenida',
+    skills: 'Habilidades',
+    portfolio: 'Portfolio',
+    contact: 'Contacto'
+  },
+  skills: {
+    fron: 'Front',
+    back: 'Backend',
+    design: 'Diseño',
+    tools: 'Herramientas'
+  },
+  occupation: 'Web/Mobile Developer - Frontend',
+  desc: 'Casi una década picando código alucinante. Fullstack MERN/MEVN',
+  projects: {
+   akkount: {
+    title: 'Akkount',
+    desc: 'App móvil para gestionar tus ahorros'
+   },
+   cheetty: {
+    title: 'Cheetty',
+    desc: 'Web para almacenar cheetsheets'
+   },
+   lionmiss: {
+    title: 'LionMiss',
+    desc: 'Back de una tienda online de ropa'
+   },
+   codetest: {
+    title: 'Code\'s Pieces',
+    desc: 'Diversos ejemplos de código'
+   },
+   placeholder: {
+    name: 'Mi nombre es...',
+    mail: 'mail@mail.es',
+    msg: 'felicitaciones, propuestas, críticas, bla, bla...'
+   },
+   labels: {
+    send: 'Envío'
+   }
+  },
+  msg: {
+    mailSuc: '😃 ¡Mensaje enviado!',
+    mailFail: '☹️ Hubo un error. Inténtelo más tarde.'
+  }
 }
