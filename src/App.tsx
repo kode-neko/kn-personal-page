dd20531b2ca642a77779f5dc2dcea84ccc3d9a49
Kodeneko <ladysun.freedom@gmail.com>
2024-06-09 18:53:26 +0200

    [migration] Home page minimal style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { RouterProvider } from "react-router-dom";
+import router from './router'
+import './globals.css'
+
 function App() {
   return (
-    <>
-      Main Jarlls
-    </>
+    <RouterProvider router={router} />
   )
 }
 
--- src/layouts/main-layout/MainLayout.tsx
+++ src/layouts/main-layout/MainLayout.tsx
@@ -1,4 +1,4 @@
-import styles from './styles.module.css'
+import styles from './styles.module.less'
 
 const MainLayout = ({children}: React.PropsWithChildren) => {
   return (
