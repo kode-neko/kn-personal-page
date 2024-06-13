db1a80917e860e7571337ee6f408c2e2757b86de
Kodeneko <ladysun.freedom@gmail.com>
2024-06-13 14:11:47 +0200

    [compos] sw lang

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
-export {BtnIcon,Field,SubTitle,Tag} from './atoms'
+export {BtnIcon,Field,SubTitle,Tag,SwLang} from './atoms'
 export {ContactForm} from './molecules'
 export {ContactSection,Footer,FooterFloat,MainBar,PortfolioSection,SkillsSection,WelcomeSection} from './organisms'
