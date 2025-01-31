0ce92c4e64127fae4dda6b92c82d5575f1ccc5f9
Kodeneko <ladysun.freedom@gmail.com>
2025-01-31 18:55:14 +0100

    [main] Fixed service contact

--- src/services/getContact.ts
+++ src/services/getContact.ts
@@ -2,9 +2,10 @@ import axios from 'axios';
 import { Contact } from '../model';
 import { serviceContact } from '../globals';
 
-const {VITE_CONTACT_SEND} = process.env
+const { VITE_CONTACT_SEND } = import.meta.env;
 
 function sendMsg(contact: Contact) {
+  console.log(`${VITE_CONTACT_SEND}${serviceContact}`)
   return axios.post(`${VITE_CONTACT_SEND}${serviceContact}`, contact)
 }
 
